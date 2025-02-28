e365073e2de53756ad81700d194ecfb7dea482ed
Mohit <mohitarora2435@gmail.com>
2025-03-01 01:01:10 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./index.css";
-import Bottom from "./page/bottom.jsx";
-import Footer from "./page/footer.jsx";
-import Hero from "./page/hero.jsx";
-import Middle from "./page/middle.jsx";
+import Bottom from "./page/Bottom.jsx";
+import Footer from "./page/Footer.jsx";
+import Hero from "./page/Hero.jsx";
+import Middle from "./page/Middle.jsx";
 import Middle2 from "./page/Middle2.jsx";
 import Middle3 from "./page/Middle3.jsx"
 
